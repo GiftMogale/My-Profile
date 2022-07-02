c809d72d2fbcfcc7a82a070bf55ed9f35d37e423
unknown <giftmangena222@gmail.com>
2022-07-02 16:41:01 +0200

    profile-reactjs

--- src/components/footer/footer.jsx
+++ src/components/footer/footer.jsx
@@ -14,8 +14,8 @@ const footer = () => {
 
     <ul className='permalinks'>
       <li> <a href='#'>Home</a></li>
-      <li> <a href='#about'>About</a></li>
-      <li> <a href='#experience'>Experience</a></li>
+      <li> <a href='#about'>About Me</a></li>
+      <li> <a href='#experience'>My Experience</a></li>
       <li> <a href='#portfolio'>Portfolio</a></li>
       <li> <a href='#contact'>Contact Me</a></li>
     </ul>
